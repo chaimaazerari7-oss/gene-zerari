f20239728dfc4aa2a3de03fe9874e04c0a6922e5
chaimaazerari7-oss <chaimaazerari7@gmail.com>
2025-12-13 10:34:14 +0100

    gene _zerari py
    zerari chaimaa et snouci hanene etdib djazia et benallal waffaa et benali riham

--- main (7).py
+++ main (7).py
@@ -23,6 +23,55 @@ print("**************** Creation et affichage *****************")
 print("Tableau des séquences ADN :\n")
 print(df)
 print("\n")
+-------------------------------
+# 3 et 4) Filtrage et calcul de la moyenne
+# ----------------------------------------------------
+
+print("************ Filtrage selon le pourcentage GC *************")
+print("#3) Filtrer les séquences avec un pourcentage de GC supérieur à 50%")
+filtered_df = df[df["Pourcentage GC"] > 50]
+print(filtered_df)
+print("\n\n")
+
+# #4) Calcul de la moyenne du pourcentage de GC
+print("************ Calcul de la moyenne *************")
+print("#4) Calculer la moyenne du pourcentage de GC")
+average_gc = df["Pourcentage GC"].mean()
+print(f"Pourcentage moyen de GC : {average_gc:.2f}%")
+print("\n\n")
+
+# ----------------------------------------------------
+# 5) Ajout de nouvelles colonnes
+# ----------------------------------------------------
+print("************ Ajouter une nouvelle colonne avec des calculs *************")
+print("#5) Ajouter une nouvelle colonne basée sur la longueur")
+
+# Ajouter une colonne catégorisant les longueurs
+print("# Ajouter une colonne 'Catégorie Longueur'")
+df["Catégorie Longueur"] = df["Longueur"].apply(lambda x: "Longue" if x > 10 else "Courte")
+print(df)
+print("\n")
+
+# #6) Ajouter une colonne comptant le nombre de 'A'
+print("#6) Ajouter une colonne comptant les 'A'")
+df["Nombre de A"] = df["Séquence"].str.count("A")
+print("Nombre de A ajoutés")
+print(df)
+print("\n\n")
+
+# ----------------------------------------------------
+# 7) Sauvegarde et chargement des données
+# ----------------------------------------------------
+print("************ Sauvegarder et charger les données avec pandas *************")
+
+# Sauvegarder le DataFrame dans un fichier CSV
+# df.to_csv("tableau_sequences.csv", index=False)
+# print("Le DataFrame a été sauvegardé dans un fichier CSV.")
+# print("\n")
+
+
+
+إ
 
 print("************** Operations ***************")
 # 2) Sélectionner et afficher uniquement la colonne “Longueur”
